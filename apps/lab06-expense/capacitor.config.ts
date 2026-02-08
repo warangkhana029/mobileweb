6981638d20bafc5bad3c381d0cc69c7cfb78fbed
warangkhana029 <warangkhana.p@kkumail.com>
2026-02-08 20:16:40 +0700

    yeb

--- apps/lab06-expense/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'lab06-expense',
-  webDir: 'dist'
-};
-
-export default config;
