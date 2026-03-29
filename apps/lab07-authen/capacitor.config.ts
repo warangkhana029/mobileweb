e9586d717e419fae4658ad639afa23efac118de9
warangkhana029 <warangkhana.p@kkumail.com>
2026-03-29 18:53:02 +0700

    wow

--- apps/lab07-authen/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'cpkku.shemin.lab07authen',
-  appName: 'lab07-authen',
-  webDir: 'dist'
-};
-
-export default config;
--- apps/lab07-authen/src/auth/auth-service.ts
+++
@@ -1,9 +0,0 @@
-import { Capacitor } from "@capacitor/core";
-import { IAuthService } from "./auth-interface";
-import { FirebaseWebAuthService } from "./auth-web";
-import { FirebaseAppAuthService } from "./auth-app";
-
-export const authService: IAuthService =
-  Capacitor.isNativePlatform()
-    ? new FirebaseAppAuthService()
-    : new FirebaseWebAuthService();
